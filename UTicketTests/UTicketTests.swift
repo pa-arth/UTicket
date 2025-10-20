440690091e3de1e81f28e29486ba7e25a9f3a80a
Paarth <paarthguardian@gmail.com>
2025-10-20 12:10:06 -0500

    Initial Commit

---
+++ UTicketTests/UTicketTests.swift
@@ -0,0 +1,17 @@
+//
+//  UTicketTests.swift
+//  UTicketTests
+//
+//  Created by Paarth Jamdagneya on 10/20/25.
+//
+
+import Testing
+@testable import UTicket
+
+struct UTicketTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
